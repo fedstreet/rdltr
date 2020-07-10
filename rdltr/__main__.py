0214039fafa6bc58ebc856a562330319cc4913f2
fedstreet <leontyev.ol2@gmail.com>
2020-07-10 18:26:12 +0300

    Replace gunicorn with waitress to run in Windows

--- rdltr/__main__.py
+++ rdltr/__main__.py
@@ -1,9 +1,8 @@
-# source: http://docs.gunicorn.org/en/stable/custom.html
 import multiprocessing
 import os
 
-import gunicorn.app.base
 from flask_migrate import upgrade
+from waitress import serve
 from rdltr import create_app
 
 
@@ -20,33 +19,13 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 app = create_app()
 
 
-class StandaloneApplication(gunicorn.app.base.BaseApplication):
-    def __init__(self, current_app, options=None):
-        self.options = options or {}
-        self.application = current_app
-        super().__init__()
-
-    def load_config(self):
-        config = {
-            key: value
-            for key, value in self.options.items()
-            if key in self.cfg.settings and value is not None
-        }
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
-
-
 def upgrade_db():
     with app.app_context():
         upgrade(directory=BASEDIR + '/migrations')
 
 
 def main():
-    options = {'bind': f"{HOST}:{PORT}", 'workers': WORKERS}
-    StandaloneApplication(app, options).run()
+    serve(app, listen=f"{HOST}:{PORT}", threads=WORKERS)
 
 
 if __name__ == '__main__':
